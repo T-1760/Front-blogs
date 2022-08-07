d1f85e21e7cf89f393a15928adab4df1b90921aa
variable <994181995@qq.com>
2022-08-07 18:06:42 +0800

    【部署项目】pubilc部署目录内容 路径 修复 test_001

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,7 @@ const friendLink = require("./friendLink.js")
 module.exports = {
   "title": "vue-press-blogs",
   "description": "What a impressive, this is my front blogs!",
-  "base": "/public/",
+  "base": "/",
   "dest": "./public",
   "head": [
     [ "link", { "rel": "icon", "href": "/favicon.ico" }],
